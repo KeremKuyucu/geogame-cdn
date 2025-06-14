cc5969fcc6c0b2b493e21f16de1e3d11daf70296
Kerem <kkuyucu2254@gmail.com>
2025-06-14 18:32:19 +0300

    docs(layout): update page title to 'GeoGame Cdn Service'

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Kerem_KK Cdn Service',
+  title: 'GeoGame Cdn Service',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
